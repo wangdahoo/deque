9c15c81f36905e2a96a44e2af3e9fcfdcd4d3943
wangdahoo <157195705@qq.com>
2017-10-10 16:23:36 +0800

    update

--- test/index.js
+++ test/index.js
@@ -1,10 +1,10 @@
+const assert = require('assert')
 const Deque = require('../')
 
 describe('module', () => {
   describe('Deque', () => {
     it('Deque() should success', () => {
       const deque = new Deque([3.14, 'hello', false])
-
       const dequeIt = deque.iterator()
 
       let next = { done: false }
@@ -12,6 +12,12 @@ describe('module', () => {
         next = dequeIt.next()
         console.log(next.value)
       }
+
+      deque.shift()
+      deque.push(200)
+
+      assert.equal(deque.head.data, 'hello', '头元素错误')
+      assert.equal(deque.tail.data, 200, '尾元素错误')
     })
   })
 })
